173e379759b7fda47689ad952243c09993bb99f1
Gabriel Ulbrich <gabriel.ulbrich@mirumagency.com>
2020-09-10 21:56:18 -0300

    add Prime View and Login page.

---
+++ src/web/vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	pages: {
+		index : {
+			entry: 'src/pages/Login/main.js',
+			template: 'public/index.html',
+			filename: 'index.html',
+			title: 'INDEX',
+			chunks: ['chunk-vendors', 'chunk-common', 'index']
+		},
+		login : {
+			entry: 'src/pages/Login/main.js',
+			template: 'public/login.html',
+			filename: 'login.html',
+			title: 'Login',
+			chunks: ['chunk-vendors', 'chunk-common', 'login']
+		},
+		crm : {
+			entry: 'src/pages/Crm/main.js',
+			template: 'public/crm.html',
+			filename: 'crm.html',
+			title: 'CRM',
+			chunks: ['chunk-vendors', 'chunk-common', 'crm']
+		}
+	},
+	publicPath: process.env.BASE_URL// === 'production' ? '/sigma-vue' : '/'
+}
