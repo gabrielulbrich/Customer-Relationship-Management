3b9afc615e06effcdeee2e0421e8ebc19a163cba
Gabriel <gabriel_ulbrich@outlook.com>
2020-09-15 21:30:02 -0300

    Adicionar testes unitatios a aplicacao

--- src/web/.eslintrc.js
+++ src/web/.eslintrc.js
@@ -1,18 +1,34 @@
 module.exports = {
-	root: true,
-	env: {
+    root: true,
+
+    env: {
 		node: true
 	},
-	'extends': [
+
+    'extends': [
 		'plugin:vue/essential',
 		'eslint:recommended'
 	],
-	rules: {
+
+    rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
 	},
-	parserOptions: {
+
+    parserOptions: {
 		parser: 'babel-eslint'
-	}
+	},
+
+    overrides: [
+      {
+        files: [
+          '**/__tests__/*.{j,t}s?(x)',
+          '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        ],
+        env: {
+          jest: true
+        }
+      }
+    ]
 }
   
