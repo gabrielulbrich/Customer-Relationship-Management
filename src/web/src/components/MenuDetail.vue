790445b56e29f046f2585ac7b6b2978f162497f4
Gabriel <gabriel_ulbrich@outlook.com>
2020-10-15 00:59:13 -0300

    Add Menu de detalhe para leads na pagina Board.

---
+++ src/web/src/components/MenuDetail.vue
@@ -0,0 +1,50 @@
+<template>
+	<div id="layout-config" :class="containerClass">
+		<a href="#" class="layout-config-close" @click="hideConfigurator">
+			<i class="pi pi-times"></i>
+		</a>
+
+		<div class="layout-config-content">
+
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			active: {
+				type: Boolean,
+				default: false
+			},
+		},
+		data() {
+			return {
+				d_active: this.active,
+			}
+		},
+		watch: {
+			$route() {
+				if (this.d_active) {
+					this.d_active = false;
+				}
+			},
+			active() {
+				this.d_active = !this.d_active
+			}
+		},
+		outsideClickListener: null,
+		methods: {
+			hideConfigurator(event) {
+				this.d_active = false;
+				this.unbindOutsideClickListener();
+				event.preventDefault();
+			},
+		},
+		computed: {
+			containerClass() {
+				return ['layout-config', {'layout-config-active': this.d_active}];
+			},
+		}
+	}
+</script>
--- src/web/src/views/Crm/App.vue
+++ src/web/src/views/Crm/App.vue
@@ -19,8 +19,6 @@
 			<router-view />
 		</div>
 
-		<AppConfig :layoutMode="layoutMode" :layoutColorMode="layoutColorMode" @layout-change="onLayoutChange" @layout-color-change="onLayoutColorChange"/>
-
 		<AppFooter />
 	</div>
 </template>
@@ -29,7 +27,6 @@
 import AppTopBar from './AppTopbar.vue';
 import AppProfile from './AppProfile.vue';
 import AppMenu from './AppMenu.vue';
-import AppConfig from './AppConfig.vue';
 import AppFooter from './AppFooter.vue';
 
 export default {
@@ -41,7 +38,8 @@ export default {
             overlayMenuActive: false,
             mobileMenuActive: false,
             menu : [
-                {label: 'Dashboard', icon: 'pi pi-fw pi-home', to: this.$store.getters.pageName },
+                {label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/'+this.$store.getters.pageName },
+                {label: 'Board', icon: 'pi pi-fw pi-table', to: this.$store.getters.pageName+'/board'},
                 {label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/chart'},
                 {label: 'View Source', icon: 'pi pi-fw pi-search', command: () => {window.location = "https://www.primefaces.org/sigma-vue/#/"}}
             ]
@@ -160,7 +158,6 @@ export default {
         'AppTopBar': AppTopBar,
         'AppProfile': AppProfile,
         'AppMenu': AppMenu,
-        'AppConfig': AppConfig,
         'AppFooter': AppFooter,
     }
 }
