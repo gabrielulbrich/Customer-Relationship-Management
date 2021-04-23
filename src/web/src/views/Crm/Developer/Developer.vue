e027e7f73a602aa76b572fe050cdfd90754acc10
Gabriel <gabriel_ulbrich@outlook.com>
2021-04-23 20:36:06 -0300

    Listing Apis Feature

--- src/web/src/views/Crm/Developer/Developer.vue
+++ src/web/src/views/Crm/Developer/Developer.vue
@@ -6,10 +6,24 @@
 				<Accordion>
           <div v-for="(api, index) in apis" :key="index">
             <AccordionTab :header="api.api">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <DataTable :value="api.fields" editMode="cell" class="editable-cells-table" sortMode="single" :sortOrder="1">
+                <Column field="name" header="Campo">
+                  <template #editor="slotProps">
+                    <InputText type="text" placeholder="Label" v-model="slotProps.data.name"></InputText>
+                  </template>
+                </Column>
+                <Column field="type.name" header="Tipo">
+                  <template #editor="slotProps">
+                    <Dropdown v-model="slotProps.data.type" :options="typeValues" optionLabel="name" placeholder="Select" />
+                  </template>
+                </Column>
+                <Column field="description" header="Descrição">
+                  <template #editor="slotProps">
+                    <Textarea placeholder="Your Message" v-model="slotProps.data.description" :autoResize="true" rows="3" cols="30" />
+                  </template>
+                </Column>
+              </DataTable>
+				      <Button label="Salvar" class="p-mr-2 p-mb-2"/>
             </AccordionTab>
           </div>
 				</Accordion>
@@ -25,12 +39,62 @@ import { api } from "@/services.js";
 export default {
 	data() {
 		return {
-      apis: [],
+      apis: [
+        {
+          "id":1,
+          "api":"\/site\/form",
+          "fields": [
+            {
+              "name": "Test",
+              "type": {
+                "name":"Texto",
+                "code": "text"
+              },
+              "description": "Descrição sobre o campo.",
+            },
+            {
+              "name": "Test2",
+              "type": {
+                "name":"Numérico",
+                "code": "numeric"
+              },
+              "description": "Descrição sobre o campo 2.",
+            },
+          ],
+          "pivot": {
+            "page_id":1,
+            "api_id":1
+          }
+        },
+        {
+          "id":2,
+          "api":"\/test\/qualquer",
+          "fields": [
+            {
+              "name": "Test",
+              "type": {
+                "name":"Texto",
+                "code": "text"
+              },
+              "description": "Descrição sobre o campo.",
+            },
+          ],
+          "pivot":{ 
+            "page_id":1,
+            "api_id":2
+            }
+          }
+      ],
+      typeValues: [
+          {name: 'Texto', code: 'text'},
+					{name: 'Numérico', code: 'numeric'},
+      ],
+      editingRows: [],
 			errors: [],
 		}
 	},
 	created() {
-    this.loadApis();
+    // this.loadApis();
 	},
 	mounted() {
 	},
@@ -42,6 +106,14 @@ export default {
         console.log(this.apis)
 			})
 		},
+    onRowEditInit(event) {
+      return 'ok';
+      // this.originalRows[event.index] = {...this.products3[event.index]};
+    },
+    onRowEditCancel(event) {
+      return 'bah';
+      // this.products3[event.index] = this.originalRows[event.index];
+    },
 	}
 }
 </script>
