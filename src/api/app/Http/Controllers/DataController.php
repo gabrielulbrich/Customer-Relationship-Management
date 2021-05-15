b7095a2b748d9a527551ebaf9146861dab7c47d3
Gabriel <gabriel_ulbrich@outlook.com>
2021-05-15 19:34:53 -0300

    data feature

---
+++ src/api/app/Http/Controllers/DataController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Page;
+use App\Data;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+//use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
+use  App\User;
+use  App\Api;
+
+class DataController extends Controller
+{
+
+}
--- src/web/src/views/Crm/Data/Data.vue
+++ src/web/src/views/Crm/Data/Data.vue
@@ -1,8 +1,34 @@
 <template>
 	<div class="p-grid crud-demo">
-    <p>DATAAAA</p>
-	</div>
+    <Toast position="bottom-right" />
+		<div class="p-col-12">
+			<div class="card">
+				<h5>Dados Salvos</h5>
+				<Accordion>
+          <div v-for="(api, index) in apis" :key="index">
+            <AccordionTab>
+              <template #header>
+                <div class="p-d-flex p-jc-between" style="width: 100%">
+                  <span>/{{pageName}}/{{ api.api }}</span>
+                </div>
+              </template>
+              <DataTable :value="api.fields" editMode="cell" class="editable-cells-table" sortMode="single" :sortOrder="1">
+                <Column field="name" header="Campo">
 
+                </Column>
+                <Column field="type.name" header="Tipo">
+
+                </Column>
+                <Column field="description" header="Descrição">
+
+                </Column>
+              </DataTable>
+            </AccordionTab>
+          </div>
+				</Accordion>
+			</div>
+    </div>
+	</div>
 </template>
 
 <script>
@@ -12,6 +38,8 @@ import { mapGetters } from 'vuex';
 export default {
 	data() {
 		return {
+      apis: [],
+      errors: [],
     }
 	},
 	created() {
@@ -20,11 +48,19 @@ export default {
 	mounted() {
 	},
 	methods: {
-		
+    loadApis() {
+			api.get("/api/get-all")
+			.then((response) => {
+        response.data.apis.forEach(e => {
+          return e.fields = JSON.parse(e.fields)
+        });
+				this.apis = response.data.apis;
+			})
+		},
 	},
   computed: {
     ...mapGetters([
-      'pageName',
+    'pageName',
     ]),
   }
 }
